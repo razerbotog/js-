bacd27afe0bdef6aba3beedf1bd86dbaf05da3d6
Arif akhtar <123809151+razerbotog@users.noreply.github.com>
2023-09-13 18:35:34 +0000

    datatypes 2

--- basics/dataTypes2.js
+++ basics/dataTypes2.js
@@ -2,3 +2,24 @@
 // 7 types: string,number,boolean,null,undefined,symbol,bigInt
 
 // reference (non-primitive)
+// 3 types: array, objects, functions
+const array = ["Arif", "Arif2", "Arif3",] // array
+
+const object = {
+    name:"arif",
+    height:6,
+    favouriteColor:"blue"
+} // object
+
+const myFunction = function() {
+    console.log("Hello");
+}
+
+myFunction();
+
+
+
+const person = () => {
+    console.log("this is  a function");
+} //arrow function
+person();
