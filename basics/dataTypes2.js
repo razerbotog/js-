1992f1b4cc040e91f13eaddd8c4d0c67346844e4
Arif akhtar <123809151+razerbotog@users.noreply.github.com>
2023-09-16 15:14:04 +0000

    stack and heap memory

--- basics/dataTypes2.js
+++ basics/dataTypes2.js
@@ -23,3 +23,26 @@ const person = () => {
     console.log("this is  a function");
 } //arrow function
 person();
+
+/* memory in js */
+// stack : use by primitive data types (the original value doesn't change but we change the copied version)
+// heap: use by reference (non-primitive) data types (the original value can be changed bcoz it use original version)
+
+// ------
+let myDomain = "domain.com";
+
+let myAnotherDomain = myDomain;
+console.log(myAnotherDomain);
+
+myAnotherDomain = "domain1.com"
+console.table([myDomain, myAnotherDomain]) // only changed to which we applied
+// ------
+
+let userone = {
+    email: "userone@gmail.com",
+    upi: "user@ybl"
+}
+let usertwo = userone;
+usertwo.email = "usertwo@gmail.com";
+
+console.table([userone.email, usertwo.email]); //both email got changed
