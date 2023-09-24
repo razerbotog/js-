7fae2feea62e779a7faef8c3efea8d93798cf9b0
Arif akhtar <123809151+razerbotog@users.noreply.github.com>
2023-09-24 18:24:23 +0000

    BTS(Behind The Seen)

---
+++ basics/BTS.js
@@ -0,0 +1,17 @@
+// BTS - Behind The Seen
+
+// How does js execute code
+//
+
+//the code
+let val1 = 10;
+let val2 = 5;
+function addNum (num1, num2){
+    let total = num1 + num2
+    return total
+}
+let result1 = addNum(val1, val2)
+let result2 = addNum(10, 2)
+
+
+console.log(result1, result2); // 15 12
