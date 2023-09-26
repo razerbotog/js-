2464ffd5f62679e4891af3e28e159987910fc237
Arif akhtar <123809151+razerbotog@users.noreply.github.com>
2023-09-26 16:04:02 +0000

    operator

---
+++ control flow/operators.js
@@ -0,0 +1,21 @@
+// Types of JavaScript Operators
+// There are different types of JavaScript operators:
+
+// Arithmetic Operators
+// Assignment Operators
+// Comparison Operators
+// String Operators
+// Logical Operators
+// Bitwise Operators
+// Ternary Operators
+// Type Operators
+
+// <
+// >
+// <=
+// >=
+// ==
+// !=
+// ===
+// !==
+// 1 + 2 , the + sign is an operator and 1 is left side operand and 2 is right side operand.
