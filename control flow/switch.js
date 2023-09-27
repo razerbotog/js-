296d5ff4a9990519a6a24d70a9bfd9b816869ddd
Arif akhtar <123809151+razerbotog@users.noreply.github.com>
2023-09-27 19:09:54 +0000

    switch

---
+++ control flow/switch.js
@@ -0,0 +1,27 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3;
+switch (month) {
+    case "dec":
+        console.log("dec")
+    case 1:
+        console.log('January');
+        break;
+    case 2:
+        console.log('Febuary');
+        break;
+    case 3:
+        console.log('March'); //March
+        break;
+
+    default:
+        console.log('No Case');
+        break;
+}
