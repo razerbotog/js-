48e83d6e8ec7c76b88dcaf48458ff435de08af01
Arif akhtar <123809151+razerbotog@users.noreply.github.com>
2023-10-01 07:41:11 +0000

    update in while_loop

--- iterations/while_loop.js
+++ iterations/while_loop.js
@@ -1,6 +1,14 @@
 //syntax 
-const i = 1
+let i = 0
 while(i <= 10){
     console.log(i);
-    i++;
+    i+= 2;
+} 
+
+let myArr = ["batman", "flash", "superman"];
+let arr = 0;
+while(arr < myArr.length){
+    console.log(myArr[arr]);
+    arr++
 }
+
