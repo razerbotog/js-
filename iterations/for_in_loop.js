1cd8d1929fe4bb149c96eb26b0fa98627e35c19e
Arif akhtar <123809151+razerbotog@users.noreply.github.com>
2023-10-02 13:27:25 +0000

    updated

--- iterations/for_in_loop.js
+++ iterations/for_in_loop.js
@@ -1,3 +1,4 @@
+// in object
 const myObject = {
     js: "javascript",
     cpp: 'C++',
@@ -9,4 +10,13 @@ for (const i in myObject) {
 //    console.log(i); //to get 'key' only
 //    console.log(myObject[i]); // to get 'value' only
    console.log(`${i} :- ${myObject[i]}`); // to get key and value
+}
+
+// in array
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for(const key in programming){
+    // console.log([key]);  //to get 'key' only
+    console.log(programming[key]); // to get 'value'
 }
